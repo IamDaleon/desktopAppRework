05ebac4cb253a4e8563b91b916f4066e9f6c06d8
iamdaleon <daleon86@gmail.com>
2023-06-02 18:37:22 -0500

    The start of an Electron App Remake

---
+++ main.js
@@ -0,0 +1,24 @@
+// include the Node.js 'path' module at the top of your file
+const path = require('path')
+
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+      width: 1200,
+      height: 800,
+      webPreferences: {
+        preload: path.join(__dirname, 'preload.js')
+      }
+    })
+  
+    win.loadFile('index.html')
+  }
+
+app.whenReady().then(() => {
+    createWindow()
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
