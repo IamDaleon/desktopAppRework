a405dcfa10145299f08d4bf494889ec9c6f8e8e6
iamdaleon <daleon86@gmail.com>
2023-06-02 19:59:54 -0500

    Daily wrap up

--- main.js
+++ main.js
@@ -1,24 +1,35 @@
-// include the Node.js 'path' module at the top of your file
-const path = require('path')
-
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
 
 const createWindow = () => {
     const win = new BrowserWindow({
       width: 1200,
       height: 800,
       webPreferences: {
-        preload: path.join(__dirname, 'preload.js')
-      }
+          preload: path.join(__dirname, 'preload.js')
+        }
     })
+    // win.loadURL('https://github.com')
   
+    // win.loadFile('collections.html')
     win.loadFile('index.html')
   }
 
 app.whenReady().then(() => {
+    ipcMain.handle('ping', () => 'pong')
     createWindow()
+
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+    }
+  })
 })
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
+    if (process.platform !== 'darwin') {
+        app.quit()
+}
 })
+
